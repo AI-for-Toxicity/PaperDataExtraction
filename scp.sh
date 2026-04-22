f1d2e7f7dbdb965c46e57845bc7572c7b0b18ba4
davide-lugli <davidelugli1@gmail.com>
2026-04-22 03:25:56 +0200

    results all models

--- scp.sh
+++ scp.sh
@@ -1,1 +1,1 @@
-scp -r -P 22055 -i ~/.ssh/id_ed25519 root@69.30.85.101:/workspace/dave/PaperDataExtraction/mistral_instr ./
+scp -r -P 22074 -i ~/.ssh/id_ed25519 root@69.30.85.101:/workspace/dave/PaperDataExtraction/llamatrain ./
